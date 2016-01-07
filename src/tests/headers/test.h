1d17cd739fb60fae5b65132c0e5093735137d2c5
Rémi Bèges <remi.beges@gmail.com>
2016-01-07 17:28:37 +0100

    added a few tests

---
+++ src/tests/headers/test.h
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
