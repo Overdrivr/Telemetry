6d8a34a8d77fc2530feb93fd31e2b4f96866476c
Rémi Bèges <remi.beges@gmail.com>
2016-01-11 17:35:47 +0100

    scaffolded methods

---
+++ src/framing/c/framing.c
@@ -0,0 +1,46 @@
+#include "framing.h"
+
+void initialize_framing()
+{
+
+}
+
+void outgoing_storage(uint8_t * buf, uint32_t bufSize)
+{
+
+}
+
+void begin()
+{
+
+}
+
+void append(uint8_t byte)
+{
+
+}
+
+void end()
+{
+
+}
+
+void incoming_storage(uint8_t * buf, uint32_t bufSize)
+{
+
+}
+
+void set_on_incoming_frame(void (*callback)(uint8_t * storage, uint32_t occupiedSize))
+{
+
+}
+
+void set_on_incoming_error(void (*callback)(int32_t errCode))
+{
+
+}
+
+void feed(uint8_t byte)
+{
+  
+}
