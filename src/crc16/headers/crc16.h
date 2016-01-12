585ad13d37f86a5a1c2bdf7cf2e68bd92c127eda
Rémi Bèges <remi.beges@gmail.com>
2016-01-12 10:21:36 +0100

    Interface

---
+++ src/crc16/headers/crc16.h
@@ -0,0 +1,11 @@
+#ifndef CRC16_H_
+#define CRC16_H_
+
+//From : https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+
+#include <stdint.h>
+
+uint16_t crc16(uint8_t * data, uint32_t lenght);
+uint16_t crc16_recursive(uint8_t byte, uint32_t remainder);
+
+#endif
