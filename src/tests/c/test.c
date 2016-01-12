516bd5cb9d609afc9327f0c61596a9d5b06104d9
Rémi Bèges <remi.beges@gmail.com>
2016-01-12 14:14:20 +0100

    setup new suite
    + cleanup

--- src/tests/c/test.c
+++ src/tests/c/test.c
@@ -2,18 +2,15 @@
 #include "telemetry.h"
 
 extern SUITE(standard_flow_suite);
+extern SUITE(emplace_message_suite);
 
-/* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
-    GREATEST_MAIN_BEGIN();      /* command-line arguments, initialization. */
+    GREATEST_MAIN_BEGIN();
 
-    /* Individual tests can be run directly. */
-    /* RUN_TEST(x_should_equal_1); */
-
-    /* Tests can also be gathered into test suites. */
     RUN_SUITE(standard_flow_suite);
+    RUN_SUITE(emplace_message_suite);
 
-    GREATEST_MAIN_END();        /* display results */
+    GREATEST_MAIN_END();
 }
