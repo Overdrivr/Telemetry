c6bba0d2389fc81db1904beb5a67abed12cdd2e2
Rémi Bèges <remi.beges@gmail.com>
2016-01-12 11:50:41 +0100

    implemented init_telemetry and emplace
    +minor changes

--- src/telemetry/c/telemetry.c
+++ src/telemetry/c/telemetry.c
@@ -1,46 +1,62 @@
 #include "telemetry.h"
+#include "string.h"
 
-void initTelemetry(TM_state* s)
+static TM_state * statePtr;
+static TM_transport * transportPtr;
+
+void init_telemetry(TM_state* s, TM_transport * t)
+{
+  statePtr = s;
+  transportPtr = t;
+}
+
+uint32_t emplace(TM_msg* m, char * buf, size_t bufSize)
+{
+  if(m->type != TM_string)
+    return 0;
+
+  uint32_t size = m->size;
+
+  if(bufSize - 1 < size)
+    size = bufSize - 1;
+
+  strncpy(buf, (char*)(m->buffer), size);
+  buf[size] = '\0';
+
+  return size;
+}
+
+uint32_t emplace_u8(TM_msg* m, uint8_t* dst)
 {
 
 }
 
-int emplace(TM_msg* m, char * buf, size_t bufSize)
+uint32_t emplace_u16(TM_msg* m, uint16_t* dst)
 {
 
 }
 
-int emplace_u8(TM_msg* m, uint8_t* dst)
+uint32_t emplace_u32(TM_msg* m, uint32_t* dst)
 {
 
 }
 
-int emplace_u16(TM_msg* m, uint16_t* dst)
+uint32_t emplace_i8(TM_msg* m, int8_t* dst)
 {
 
 }
 
-int emplace_u32(TM_msg* m, uint32_t* dst)
+uint32_t emplace_i16(TM_msg* m, int16_t* dst)
 {
 
 }
 
-int emplace_i8(TM_msg* m, int8_t* dst)
+uint32_t emplace_i32(TM_msg* m, int32_t* dst)
 {
 
 }
 
-int emplace_i16(TM_msg* m, int16_t* dst)
-{
-
-}
-
-int emplace_i32(TM_msg* m, int32_t* dst)
-{
-
-}
-
-int emplace_f32(TM_msg* m, float* dst)
+uint32_t emplace_f32(TM_msg* m, float* dst)
 {
 
 }
@@ -90,7 +106,7 @@ void subscribe(char * topic, void (*callback)(TM_state* s, TM_msg* m))
 
 }
 
-void updateTelemetry(float elapsedTime)
+void update_telemetry(float elapsedTime)
 {
 
 }
