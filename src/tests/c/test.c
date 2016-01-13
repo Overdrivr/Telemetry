1162573a320741cb3e3fcca0c80440b50301722e
Rémi Bèges <remi.beges@gmail.com>
2016-01-13 15:06:58 +0100

    removed deprecated test

--- src/tests/c/test.c
+++ src/tests/c/test.c
@@ -1,7 +1,6 @@
 #include "test.h"
 #include "telemetry.h"
 
-extern SUITE(standard_flow_suite);
 extern SUITE(emplace_message_suite);
 extern SUITE(emplace_uint_suite);
 extern SUITE(emplace_int_suite);
@@ -14,7 +13,6 @@ GREATEST_MAIN_DEFS();
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
 
-    RUN_SUITE(standard_flow_suite);
     RUN_SUITE(emplace_message_suite);
     RUN_SUITE(emplace_uint_suite);
     RUN_SUITE(emplace_int_suite);
