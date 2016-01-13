9a32d73e24dae9b7dd652df58513882317a20812
Rémi Bèges <remi.beges@gmail.com>
2016-01-13 18:57:57 +0100

    updated test list

--- src/tests/c/test.c
+++ src/tests/c/test.c
@@ -7,6 +7,7 @@ extern SUITE(emplace_int_suite);
 extern SUITE(emplace_float_suite);
 extern SUITE(pub_sub_string_suite);
 extern SUITE(framing_outgoing_suite);
+extern SUITE(framing_incoming_suite);
 
 GREATEST_MAIN_DEFS();
 
@@ -17,8 +18,9 @@ int main(int argc, char **argv) {
     RUN_SUITE(emplace_uint_suite);
     RUN_SUITE(emplace_int_suite);
     RUN_SUITE(emplace_float_suite);
-    RUN_SUITE(pub_sub_string_suite);
     RUN_SUITE(framing_outgoing_suite);
+    RUN_SUITE(framing_incoming_suite);
+    RUN_SUITE(pub_sub_string_suite);
 
     GREATEST_MAIN_END();
 }
