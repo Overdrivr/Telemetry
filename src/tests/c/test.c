9f0d0cef489f014cf57030e4001426ff723d703c
Rémi Bèges <remi.beges@gmail.com>
2016-01-13 11:23:12 +0100

    test rename

--- src/tests/c/test.c
+++ src/tests/c/test.c
@@ -6,7 +6,7 @@ extern SUITE(emplace_message_suite);
 extern SUITE(emplace_uint_suite);
 extern SUITE(emplace_int_suite);
 extern SUITE(emplace_float_suite);
-extern SUITE(publish_string_suite);
+extern SUITE(pub_sub_string_suite);
 extern SUITE(framing_outgoing_suite);
 
 GREATEST_MAIN_DEFS();
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
     RUN_SUITE(emplace_uint_suite);
     RUN_SUITE(emplace_int_suite);
     RUN_SUITE(emplace_float_suite);
-    RUN_SUITE(publish_string_suite);
+    RUN_SUITE(pub_sub_string_suite);
     RUN_SUITE(framing_outgoing_suite);
 
     GREATEST_MAIN_END();
