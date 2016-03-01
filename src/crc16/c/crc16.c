cd5c053ff6fa874b6e2a2d0426ee812819a4fb9d
Lucas Mahieu <lucas.mahieu@icloud.com>
2016-03-01 08:07:46 +0100

    Update crc16.c
    Correction of 'for' loop initialization

--- src/crc16/c/crc16.c
+++ src/crc16/c/crc16.c
@@ -3,7 +3,8 @@
 uint16_t crc16(uint8_t* data, uint32_t len)
 {
 	uint16_t rem  = 0;
-	for(uint16_t i = 0 ; i < len ; i++)
+	uint16_t i=0;
+	for(i = 0 ; i < len ; i++)
 	{
 		rem = crc16_recursive(data[i],rem);
 	}
@@ -15,8 +16,8 @@ uint16_t crc16_recursive(uint8_t byte, uint16_t remainder)
 	uint16_t n = 16;
 
 	remainder  = remainder ^ (byte << (n-8));
-
-	for(uint16_t j = 1 ; j < 8 ; j++)
+	uint16_t j = 0;
+	for(j = 1 ; j < 8 ; j++)
 	{
 		if(remainder & 0x8000)
 		{
