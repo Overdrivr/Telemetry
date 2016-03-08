da9bf1614e72773044132ded1a441e64d66f3540
Rémi Bèges <remi.beges@gmail.com>
2016-03-08 18:06:46 +0100

    Fixed update_floats tests
    That were initially failing without throwing error, because test
    function seemed to call itself continuously

--- src/tests/c/update_float_suite.c
+++ src/tests/c/update_float_suite.c
@@ -5,14 +5,14 @@ TEST update_float32()
   TM_msg dummy;
   char topic[128] = "float32";
   float buf = -64;
-  dummy.type = TM_int8;
+  dummy.type = TM_float32;
   dummy.buffer = (void *)&buf;
   dummy.size = 1;
   dummy.topic = topic;
 
   float destination;
 
-  if(!update_float32(&dummy, "float32",  &destination))
+  if(!update_f32(&dummy, "float32",  &destination))
   {
     FAIL();
   }
@@ -26,14 +26,14 @@ TEST update_float32_neg()
   TM_msg dummy;
   char topic[128] = "float32_neg";
   float buf = -64;
-  dummy.type = TM_int16;
+  dummy.type = TM_float32;
   dummy.buffer = (void *)&buf;
   dummy.size = 1;
   dummy.topic = topic;
 
   float destination;
 
-  if(!update_float32(&dummy, "float32_neg",  &destination))
+  if(!update_f32(&dummy, "float32_neg",  &destination))
   {
     FAIL();
   }
@@ -48,14 +48,14 @@ TEST update_float32_decimals()
   TM_msg dummy;
   char topic[128] = "float32_decimals";
   float buf = -64.1235;
-  dummy.type = TM_int32;
+  dummy.type = TM_float32;
   dummy.buffer = (void *)&buf;
   dummy.size = 1;
   dummy.topic = topic;
 
   float destination;
 
-  if(!update_float32(&dummy, "float32_decimals",  &destination))
+  if(!update_f32(&dummy, "float32_decimals",  &destination))
   {
     FAIL();
   }
@@ -63,9 +63,6 @@ TEST update_float32_decimals()
 
   PASS();
 }
-
-
-
 
 SUITE(update_float_suite) {
   RUN_TEST(update_float32);
